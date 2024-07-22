8a021108b74b56c0884d8c13f51b1cab8772a454
suryamsp <murugesansuryamsp@gmail.com>
2024-07-22 03:13:00 -0700

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+const PORT = 4000;
+app.get("/", function (request, response) {
+  response.send("🙋‍♂️, 🌏 🎊✨🤩");
+});
+
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
